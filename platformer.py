e79272035bf0ad8cf275f89d69d0cf9cefd0461c
Gabe <yeunggabe@gmail.com>
2024-06-20 18:01:12 -0400

    added gravity

--- platformer.py
+++ platformer.py
@@ -14,6 +14,10 @@ TILE_SCALING = 0.5
 
 # Movement speed of player, in pixels per frame
 PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+
+
 
 class MyGame(arcade.Window):
     """
@@ -30,10 +34,14 @@ class MyGame(arcade.Window):
         self.player_sprite = None
         # Our physics engine
         self.physics_engine = None
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
+        # Set up the Camera
+        self.camera = arcade.Camera(self.width, self.height)
         self.scene = arcade.Scene()
 
         # Create the Sprite lists
@@ -41,7 +49,7 @@ class MyGame(arcade.Window):
         self.scene.add_sprite_list("Walls", use_spatial_hash=True)
 
         # Set up the player, specifically placing it at these coordinates.
-        image_source = ":resources:images/animated_characters/male_adventurer/maleAdventurer_fall.png"
+        image_source = "images/ceruledge-pixel.gif"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
@@ -69,8 +77,8 @@ class MyGame(arcade.Window):
             self.scene.add_sprite("Walls", wall)
         
         # Create the 'physics engine'
-        self.physics_engine = arcade.PhysicsEngineSimple(
-            self.player_sprite, self.scene.get_sprite_list("Walls")
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Walls"]
         )
 
 
@@ -78,15 +86,17 @@ class MyGame(arcade.Window):
         """Render the screen."""
         # Clear the screen to the background color
         self.clear()
-
+        # Activate our Camera
+        self.camera.use()
         # Draw our Scene
         self.scene.draw()
-        
+
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
 
         if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
         elif key == arcade.key.DOWN or key == arcade.key.S:
             self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.LEFT or key == arcade.key.A:
@@ -96,21 +106,35 @@ class MyGame(arcade.Window):
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key."""
-
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
+        if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+
+        # Don't let camera travel past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
 
     def on_update(self, delta_time):
         """Movement and game logic"""
 
         # Move the player with the physics engine
         self.physics_engine.update()
+
+        # Position the camera
+        self.center_camera_to_player()
 
 def main():
     """Main function"""
